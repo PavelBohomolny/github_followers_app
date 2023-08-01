e29010c8eb3fac6fafbaed9a02a61794362dae91
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2023-08-01 22:07:18 +0200

    Initial Commit

---
+++ GHFollowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFollowers
+//
+//  Created by Pavel Bohomolnyi on 01/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemBlue
+    }
+
+}
+
