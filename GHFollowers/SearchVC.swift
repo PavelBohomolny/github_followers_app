1ab218c39c9e24ca49837a0aed1190379b498c5d
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2023-08-02 09:27:21 +0200

    Search View Controller

---
+++ GHFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GHFollowers
+//
+//  Created by Pavel Bohomolnyi on 02/08/2023.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
