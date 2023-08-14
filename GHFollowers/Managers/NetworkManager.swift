9e63b3f24b5432ae4b25c5ed90ec53cb6c870a9f
Pavel Bohomolnyi <pavel.bohomolnyi@skopei.com>
2023-08-14 16:06:32 +0200

    Network Manager class

---
+++ GHFollowers/Managers/NetworkManager.swift
@@ -0,0 +1,12 @@
+//
+//  NetworkManager.swift
+//  GHFollowers
+//
+//  Created by Pavel Bohomolnyi on 14/08/2023.
+//
+
+import Foundation
+
+class NetworkManager {
+    let shared = NetworkManager()
+}
